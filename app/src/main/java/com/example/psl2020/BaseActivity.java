b2306d0bb6cb83ed7d5094bd0fa6b26b25889ea7
Bilaleng <nicemehar24@gmail.com>
2020-01-15 15:24:53 +0500

    Live score Live Streaming and Home activity frontend

---
+++ app/src/main/java/com/example/psl2020/BaseActivity.java
@@ -0,0 +1,88 @@
+package com.example.psl2020;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public abstract class BaseActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    protected BottomNavigationView navigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentViewId());
+
+
+        navigationView = (BottomNavigationView) findViewById(R.id.navigationView);
+        navigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        updateNavigationBarState();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        overridePendingTransition(0, 0);
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.nav_home) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        } else if (itemId == R.id.nav_matches) {
+            Intent intent=new Intent(this, LiveScoreActivity.class);
+          //  intent.putExtra( "id","Friends" );
+            startActivity(intent);
+            finish();
+        } else if (itemId == R.id.nav_fixture) {
+            Intent intent=new Intent(this, MainActivity.class);
+          //  intent.putExtra( "id","Family" );
+            startActivity(intent);
+            finish();
+        }
+        else if (itemId == R.id.nav_liveStream) {
+            startActivity(new Intent(this, LiveStreamingActivity.class));
+            finish();
+        }
+        else if (itemId == R.id.nav_more) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+
+        return true;
+    }
+    private void updateNavigationBarState(){
+        int actionId = getNavigationMenuItemId();
+        selectBottomNavigationBarItem(actionId);
+    }
+
+    void selectBottomNavigationBarItem(int itemId) {
+        Menu menu = navigationView.getMenu();
+        for (int i = 0, size = menu.size(); i < size; i++) {
+            MenuItem item = menu.getItem(i);
+            boolean shouldBeChecked = item.getItemId() == itemId;
+            if (shouldBeChecked) {
+                item.setChecked(true);
+                break;
+            }
+        }
+    }
+
+    abstract int getContentViewId();
+
+    abstract int getNavigationMenuItemId();
+
+
+
+
+}
+
--- app/src/main/java/com/example/psl2020/MainActivity.java
+++ app/src/main/java/com/example/psl2020/MainActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import hotchemi.android.rate.AppRate;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+      //  setContentView( R.layout.activity_main );
+        AppRate.with(this)
+                .setInstallDays( 0 )
+                .setLaunchTimes( 3 )
+                .setRemindInterval( 5 )
+                .monitor();
+
+        AppRate.showRateDialogIfMeetsConditions( this );
+        AppRate.with( this ).clearAgreeShowDialog();
+    }
+
+    @Override
+    int getContentViewId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    int getNavigationMenuItemId() {
+        return R.id.nav_home;
     }
 }
