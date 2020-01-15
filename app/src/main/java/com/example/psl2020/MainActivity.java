2e5020e23dc01937a8fcac8b1965b80ce869caf3
basit96 <pakkobra007@gmail.com>
2020-01-15 15:14:42 +0500

    nothing

--- app/src/main/java/com/example/psl2020/MainActivity.java
+++ app/src/main/java/com/example/psl2020/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.psl2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
     }
 }
