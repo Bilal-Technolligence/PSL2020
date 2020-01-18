58a385d5a632302abf89ff17eba28fbe1ce99dff
basit96 <pakkobra007@gmail.com>
2020-01-18 18:17:29 +0500

    add drawer

--- app/src/main/java/com/example/psl2020/BaseActivity.java
+++ app/src/main/java/com/example/psl2020/BaseActivity.java
@@ -8,27 +8,39 @@ import android.view.View;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public abstract class BaseActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, NavigationView.OnNavigationItemSelectedListener {
-    protected BottomNavigationView navigationView;
+    protected BottomNavigationView bottomNavigationView;
     protected DrawerLayout drawerLayout;
+    protected NavigationView drawerNavigationView;
     ImageView imageView;
     protected ActionBarDrawerToggle drawerToggle;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getContentViewId());
 
-        navigationView = (BottomNavigationView) findViewById(R.id.navigationView);
-        navigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
         //drawer navigation
         drawerLayout = (DrawerLayout) findViewById(R.id.drawarLayout);
@@ -38,72 +50,106 @@ public abstract class BaseActivity extends AppCompatActivity implements BottomNa
         drawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         //drawer navbar item click
-        NavigationView drawerNavigationView = (NavigationView) findViewById(R.id.drawerNavigationView);
+        drawerNavigationView = (NavigationView) findViewById(R.id.drawerNavigationView);
         drawerNavigationView.setNavigationItemSelectedListener(this);
 
         //header click navbar
         View headerview = drawerNavigationView.getHeaderView(0);
         imageView = (ImageView) headerview.findViewById(R.id.profile_image);
-    //profile img click
+        //profile img click
         imageView.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick (View v){
+            @Override
+            public void onClick(View v) {
 
-            Snackbar.make(drawerLayout, "You must have account", Snackbar.LENGTH_LONG).show();
+                Snackbar.make(drawerLayout, "You must have account", Snackbar.LENGTH_LONG).show();
+            }
+        });
+
+        //fb login button code
+        loginButton= findViewById(R.id.login_button);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.setReadPermissions(Arrays.asList("email", "public_profile"));
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
     }
-    } );
-}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onStart() {
         super.onStart();
         updateNavigationBarState();
     }
+
     @Override
     public void onPause() {
         super.onPause();
         overridePendingTransition(0, 0);
     }
 
-//    @Override
-//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//        int itemId = item.getItemId();
-//        if (itemId == R.id.nav_home) {
-//            startActivity(new Intent(this, MainActivity.class));
-//            finish();
-//        } else if (itemId == R.id.nav_matches) {
-//            Intent intent=new Intent(this, LiveScoreActivity.class);
-//          //  intent.putExtra( "id","Friends" );
-//            startActivity(intent);
-//            finish();
-//        } else if (itemId == R.id.nav_fixture) {
-//            Intent intent=new Intent(this, MainActivity.class);
-//          //  intent.putExtra( "id","Family" );
-//            startActivity(intent);
-//            finish();
-//        }
-//        else if (itemId == R.id.nav_liveStream) {
-//            startActivity(new Intent(this, LiveStreamingActivity.class));
-//            finish();
-//        }
-//        else if (itemId == R.id.nav_more) {
-//            startActivity(new Intent(this, MainActivity.class));
-//            finish();
-//        }
-//        else if(itemId==R.id.inviteFriends) {
-//            Snackbar.make(drawerLayout, "Invite Friends", Snackbar.LENGTH_LONG).show();
-//
-//        }
-//
-//        return true;
-//    }
-    private void updateNavigationBarState(){
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.nav_home) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        } else if (itemId == R.id.nav_matches) {
+            Intent intent = new Intent(this, LiveScoreActivity.class);
+            //  intent.putExtra( "id","Friends" );
+            startActivity(intent);
+            finish();
+        } else if (itemId == R.id.nav_fixture) {
+            Intent intent = new Intent(this, MainActivity.class);
+            //  intent.putExtra( "id","Family" );
+            startActivity(intent);
+            finish();
+        } else if (itemId == R.id.nav_liveStream) {
+            startActivity(new Intent(this, LiveStreamingActivity.class));
+            finish();
+        } else if (itemId == R.id.nav_more) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        } else if (itemId == R.id.inviteFriends) {
+            Snackbar.make(drawerLayout, "Invite Friends", Snackbar.LENGTH_LONG).show();
+
+        } else if (itemId == R.id.scoreboard) {
+            Snackbar.make(drawerLayout, "scoreboard", Snackbar.LENGTH_LONG).show();
+        } else if (itemId == R.id.guideline) {
+            Snackbar.make(drawerLayout, "GuideLine", Snackbar.LENGTH_LONG).show();
+        } else if (itemId == R.id.rateUs) {
+            Snackbar.make(drawerLayout, "Rate Us", Snackbar.LENGTH_LONG).show();
+        } else if (itemId == R.id.logout) {
+            Snackbar.make(drawerLayout, "Logout", Snackbar.LENGTH_LONG).show();
+        }
+
+        return true;
+    }
+
+    private void updateNavigationBarState() {
         int actionId = getNavigationMenuItemId();
         selectBottomNavigationBarItem(actionId);
     }
 
     void selectBottomNavigationBarItem(int itemId) {
-        Menu menu = navigationView.getMenu();
+        Menu menu = bottomNavigationView.getMenu();
         for (int i = 0, size = menu.size(); i < size; i++) {
             MenuItem item = menu.getItem(i);
             boolean shouldBeChecked = item.getItemId() == itemId;
@@ -124,45 +170,28 @@ public abstract class BaseActivity extends AppCompatActivity implements BottomNa
     //drawer open close click
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (drawerToggle.onOptionsItemSelected( item )) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-        return super.onOptionsItemSelected( item );
+        return super.onOptionsItemSelected(item);
     }
-
-//    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener=new BottomNavigationView.OnNavigationItemSelectedListener() {
-//        @Override
-//        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-//            switch (menuItem.getItemId()) {
-//                case R.id.nav_matches: {
-//                    Snackbar.make(drawerLayout, "invite Friends", Snackbar.LENGTH_LONG).show();
-//                    break;
-//                }
+//    @Override
+//    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//        switch (menuItem.getItemId()) {
+//            case R.id.inviteFriends: {
+//                Snackbar.make(drawerLayout, "Invite Friends", Snackbar.LENGTH_LONG).show();
+//                break;
 //            }
-//            return false;
+//            case R.id.nav_matches:{
+//                Intent intent=new Intent(this, LiveScoreActivity.class);
+//                startActivity(intent);
+//                finish();
+//
+//                break;
+//            }
 //        }
-//    };
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case R.id.inviteFriends: {
-                            Snackbar.make(drawerLayout, "Invite Friends", Snackbar.LENGTH_LONG).show();
-                break;
-            }
-            case R.id.nav_matches:{
-                Intent intent=new Intent(this, LiveScoreActivity.class);
-                startActivity(intent);
-                finish();
-//                Snackbar.make(drawerLayout, " Friends", Snackbar.LENGTH_LONG).show();
-
-                break;
-            }
-
-
-
-        }
-        return false;
-    }
+//        return false;
+//    }
 
 
 }
