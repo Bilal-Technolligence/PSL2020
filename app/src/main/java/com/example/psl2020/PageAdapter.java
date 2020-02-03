e4556ef6d81ecd86c8728fe88643eab269a65ec6
Bilaleng <nicemehar24@gmail.com>
2020-02-03 13:05:10 +0500

    Frontend Changes

---
+++ app/src/main/java/com/example/psl2020/PageAdapter.java
@@ -0,0 +1,36 @@
+package com.example.psl2020;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class PageAdapter extends FragmentStatePagerAdapter {
+    int countTab;
+    public PageAdapter(@NonNull FragmentManager fm, int countTab) {
+        super(fm);
+        this.countTab=countTab;
+    }
+
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                Fixture fixture=new Fixture();
+                return fixture;
+            case 1:
+                Scoreboard scoreboard=new Scoreboard();
+                return scoreboard;
+            default:
+                return null;
+
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return countTab;
+    }
+}
