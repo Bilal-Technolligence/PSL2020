21c5cfbc15c9fce7272851de24ca99c7808ef5a1
Abdul Basit <pakkobra007@gmail.com>
2020-02-03 18:03:08 +0500

    merger bilal + basit + aashir

--- app/src/main/java/com/example/psl2020/NewsAdapter.java
+++ app/src/main/java/com/example/psl2020/NewsAdapter.java
@@ -1,6 +1,8 @@
 package com.example.psl2020;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,11 +18,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     ArrayList<NewsDataClass> newsDataClassArray;
-    Context context;
+    Activity activity;
 
-    public NewsAdapter(ArrayList<NewsDataClass> newsDataClassArray,Context context){
+    public NewsAdapter(ArrayList<NewsDataClass> newsDataClassArray,Activity activity){
         this.newsDataClassArray=newsDataClassArray;
-        this.context=context;
+        this.activity=activity;
     }
     @NonNull
     @Override
@@ -32,11 +34,22 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        NewsDataClass newsDataClass=newsDataClassArray.get(position);
+        final NewsDataClass newsDataClass=newsDataClassArray.get(position);
         holder.newsTitle.setText(newsDataClass.getNewsTitle());
         //holder.newsDetail.setText(newsDataClass.getNewsDetails());
         holder.newsDatetime.setText(newsDataClass.getNewsDatetime());
         Picasso.get().load(newsDataClass.getImgUrl()).into(holder.newsImage);
+        holder.newsImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(activity,NewsDetail.class);
+                intent.putExtra("title",newsDataClass.getNewsTitle());
+                intent.putExtra("date",newsDataClass.getNewsDatetime());
+                intent.putExtra("link",newsDataClass.getNewsDetails());
+                intent.putExtra("imgUrl",newsDataClass.getImgUrl());
+                activity.startActivity(intent);
+            }
+        });
 
     }
 
