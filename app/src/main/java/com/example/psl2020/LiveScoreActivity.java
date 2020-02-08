175a884b03db6b87c289f0f5e1b84b1b52af88ee
Aashir-Technolligence <aashi7651@gmail.com>
2020-02-08 18:15:59 +0500

    remains 5 things

--- app/src/main/java/com/example/psl2020/LiveScoreActivity.java
+++ app/src/main/java/com/example/psl2020/LiveScoreActivity.java
@@ -154,19 +154,19 @@ public class LiveScoreActivity extends BaseActivity {
                                             elected = dataSnapshot.child("elected").getValue().toString();
 
 
-                                            if (toss == 51) {
-                                                tosswon.setText("Islamabad won the toss and elected to " + elected + " first.");
-                                            } else if (toss == 12) {
-                                                tosswon.setText("Karachi won the toss and elected to " + elected + " first.");
-                                            } else if (toss == 13) {
-                                                tosswon.setText("Lahore won the toss and elected to " + elected + " first.");
-                                            } else if (toss == 14) {
-                                                tosswon.setText("Multan won the toss and elected to " + elected + " first.");
-                                            } else if (toss == 15) {
-                                                tosswon.setText("Peshawar won the toss and elected to " + elected + " first.");
-                                            } else if (toss == 53) {
-                                                tosswon.setText("Quetta won the toss and elected to " + elected + " first.");
-                                            }
+//                                            if (toss == 51) {
+//                                                tosswon.setText("Islamabad won the toss and elected to " + elected + " first.");
+//                                            } else if (toss == 12) {
+//                                                tosswon.setText("Karachi won the toss and elected to " + elected + " first.");
+//                                            } else if (toss == 13) {
+//                                                tosswon.setText("Lahore won the toss and elected to " + elected + " first.");
+//                                            } else if (toss == 14) {
+//                                                tosswon.setText("Multan won the toss and elected to " + elected + " first.");
+//                                            } else if (toss == 15) {
+//                                                tosswon.setText("Peshawar won the toss and elected to " + elected + " first.");
+//                                            } else if (toss == 53) {
+//                                                tosswon.setText("Quetta won the toss and elected to " + elected + " first.");
+//                                            }
                                             bowler.setText(dataSnapshot.child("bowler").getValue().toString());
                                         }
                                         catch (Exception e){}
